48ccc78885aff8c6ccc7f2ad88437fa0a8ab1aa5
TimBock <tim.bock@numbers.net.au>
2017-05-04 09:28:29 +1000

    DS-1184: Adding weights and filters and various data checks

--- R/max-difflogit.R
+++ R/max-difflogit.R
@@ -1,3 +1,61 @@
+#' \code{FitMaxDiff}
+#' @description Fits a rank-ordered logit model with ties to a max-diff experiment.
+#' @param design A \code{data.frame}, where the first variable is called 'Version', the second is called 'Task',
+#' and the remaining variables contain the alternatives shown in each tas.
+#' @param version A vector of integers showing the version of the design shown to each respondent.
+#' @param best A matrix of integers showing the choices made by each respondent on each of the tasks. One column
+#' for each task. The integers need to corresponde to the \code{design} vector of integers showing the version of
+#' the design shown to each respondent. Coerced to a matrix if a \code{data.frame}.
+#' @param worst A matrix of integers showing the choice of 'worst'.
+#' @param names A character vector names of the alternatives. If only a single element is supplied, it is split by commas.
+#' @param subset An optional vector specifying a subset of observations to be
+#'   used in the fitting process.
+#' @param weights An optional vector of sampling or frequency weights.
+#' @param trace Non-negative integer indicating the detail of outputs provided during estimation: 0 indicates
+#' no outputs, and 6 is the most detailed outputs.
+#' @importFrom flipData CalibrateWeight CleanSubset CleanWeights
+#' @export
+FitMaxDiff <- function(design, version, best, worst, names, subset = NULL, weights = NULL, trace = 0)
+{
+    # Cleaning and checking data
+    n <- length(best[[1]])
+    if (!is.null(weights))
+        weights <- CleanWeights(weights)
+    subset <- CleanSubset(subset, n)
+    if (is.null(version))
+        version <- rep(1, n)
+    version <- version[subset]
+    if (!is.null(weights))
+    {
+        weights <- weights[subset]
+        weights <- CalibrateWeight(weights)
+    }
+    best <- as.data.frame(best[subset, ])
+    worst <- as.data.frame(worst[subset, ])
+    if (!("Version" %in% names(design)))
+        design <- cbind(Version = 1, design)
+    if (!("Task" %in% names(design)))
+        design <- cbind(Task = 1:nrow(design), design)
+    X <- IntegrateDesignAndData(design, version, best, worst)
+    n.alternatives <- max(design[, -1:-2])
+    if (length(names) != n.alternatives)
+        names <- strsplit(names)
+    if (length(names) != n.alternatives)
+        stop("The number of 'names' does not match the number of alternatives in the 'design'.")
+    # Estimating
+    init.b <- seq(.01,.02, length.out = n.alternatives - 1)
+    solution = optim(init.b, logLikelihoodMaxDiff,
+                     gr = NULL,
+                     X = X,
+                     weights = weights,
+                     method =  "BFGS",
+                     control = list(fnscale  = -1, maxit = 1000, trace = trace), hessian = FALSE)
+    pars = c(0, solution$par)
+    names(pars) = names
+    list(log.likelihood = solution$value, coef = pars, n = n)
+}
+
+
 #' \code{dMaxDiff}
 #' @description The density of a respondent's max-diff data.
 #' @param b A vector of parameter estimates.
@@ -14,45 +72,15 @@ dMaxDiff <- function(b, x)
 #' @description The log-likelihood for a max-diff experiment.
 #' @param b A vector of parameter estimates.
 #' @param X The experimental design for a sample (a \code{\link{list}})
-logLikelihoodMaxDiff = function(b, X)
+logLikelihoodMaxDiff = function(b, X, weights)
 {
    b[b > 100] = 100
    b[b < -100] = -100
    probs <- as.numeric(lapply(X, b = c(0, b), dMaxDiff))
-   sum(log(probs))
+   log.probs <- log(probs)
+   if (!is.null(weights))
+       log.probs <- log.probs * weights
+   sum(log.probs)
 }
-
-
-#' \code{FitMaxDiff}
-#' @description Fits a rank-ordered logit model with ties to a max-diff experiment.
-#' @param design A \code{data.frame}, where the first variable is called 'Version', the second is called 'Task',
-#' and the remaining variables contain the alternatives shown in each tas.
-#' @param version A vector of integers showing the version of the design shown to each respondent.
-#' @param best A matrix of integers showing the choices made by each respondent on each of the tasks. One column
-#' for each task. The integers need to corresponde to the \code{design} vector of integers showing the version of
-#' the design shown to each respondent. Coerced to a matrix if a \code{data.frame}.
-#' @param worst A matrix of integers showing the choice of 'worst'.
-#' @param names The names of the alternatives.
-#' @param trace Non-negative integer indicating the detail of outputs provided during estimation: 0 indicates
-#' no outputs, and 6 is the most detailed outputs.
-#'
-#' @export
-FitMaxDiff <- function(design, version, best, worst, names, trace = 0)
-{
-    X <- IntegrateDesignAndData(design, version, best, worst)
-    n.alternatives <- max(design[, -1:-2])
-    init.b <- seq(.01,.02, length.out = n.alternatives - 1)
-    solution = optim(init.b, logLikelihoodMaxDiff,
-                     gr = NULL,
-                     X = X,
-                     method =  "BFGS",
-                     control = list(fnscale  = -1, maxit = 1000, trace = trace), hessian = FALSE)
-    pars = c(0, solution$par)
-    names(pars) = names
-    #names(pars) = dimnames(stacked.data)[[2]]
-    list(log.likelihood = solution$value, coef = pars)
-}
-
-
 
 
--- tests/testthat/test-max-difflogit.R
+++ tests/testthat/test-max-difflogit.R
@@ -1,16 +1,26 @@
 context("max-diff logit")
 
+tech.data = suppressWarnings(foreign::read.spss("http://wiki.q-researchsoftware.com/images/f/f1/Technology_2017.sav", to.data.frame = TRUE))
+tech.design = read.csv("http://wiki.q-researchsoftware.com/images/7/78/Technology_MaxDiff_Design.csv")
+best = tech.data[, c("Q5a_left", "Q5b_left", "Q5c_left", "Q5d_left", "Q5e_left", "Q5f_left")]
+worst = tech.data[, c("Q5a_right", "Q5b_right", "Q5c_right", "Q5d_right", "Q5e_right", "Q5f_right")]
+names = c("Apple", "Microsoft", "IBM", "Google", "Intel", "Samsung", "Sony", "Dell", "Yahoo", "Nokia")
+
 test_that("Estimating logit parameters", {
-
-    tech.data = suppressWarnings(foreign::read.spss("http://wiki.q-researchsoftware.com/images/f/f1/Technology_2017.sav", to.data.frame = TRUE))
-    tech.design = read.csv("http://wiki.q-researchsoftware.com/images/7/78/Technology_MaxDiff_Design.csv")
-
-    best = tech.data[, c("Q5a_left", "Q5b_left", "Q5c_left", "Q5d_left", "Q5e_left", "Q5f_left")]
-    worst = tech.data[, c("Q5a_right", "Q5b_right", "Q5c_right", "Q5d_right", "Q5e_right", "Q5f_right")]
-    names = c("Apple", "Microsoft", "IBM", "Google", "Intel", "Samsung", "Sony", "Dell", "Yahoo", "Nokia")
+    # Aggregate
     result = FitMaxDiff(design = tech.design, version = rep(1, nrow(best)), best = best, worst = worst, names = names)
     q.solution <- c(0, -0.09059317085259,-1.022392901667,0.3712129560247,-0.6596444467744,0.02930605087281,-0.08321462352542,-0.870332546743,-1.105990654593,-0.8674738151854)
+    expect_equal(as.vector(result$coef), q.solution, tolerance = 0.0001)
+    # Subset
+    sub = unclass(tech.data$Q2) <= 3
+    result = FitMaxDiff(design = tech.design, version = rep(1, nrow(best)), best = best, worst = worst, subset = sub, names = names)
+    q.solution <- c(0,-0.09164874531048,-1.416097197804,0.5413188029853,-0.8428037706912,-0.5743677293267,-0.4820668863369,-0.8572557181654,-1.530467590026,-1.143659145101)
+    expect_equal(as.vector(result$coef), q.solution, tolerance = 0.0001)
+    # Subset and weight
+    wgt = tech.data$RESPNUM
+    result = FitMaxDiff(design = tech.design, version = rep(1, nrow(best)), best = best, worst = worst, subset = sub, weights = wgt, names = names)
+    q.solution <- c(0,-0.1358158415588,-1.310226780669,0.4291539729508,-0.7617988208062,-0.5332417439411,-0.4662730731195,-0.8116530486171,-1.356435800804,-1.148628546175)
     expect_equal(as.vector(result$coef), q.solution, tolerance = 0.00001)
-
+    expect_equal(result$log.likelihood, -804.218, tolerance = 0.00001)
 })
 
