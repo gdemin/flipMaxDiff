e73be77cbfe11cc0bcae3896df58ee6590a2ca19
JustinCCYap <justinyapcc@gmail.com>
2017-08-04 15:32:01 +1000

    DS-1398: use model code instead

--- tests/testthat/test-hierarchicalbayes.R
+++ tests/testthat/test-hierarchicalbayes.R
@@ -9,7 +9,7 @@ context("Hierarchical Bayes")
 # best <- tech.data[, c("Q5a_left", "Q5b_left", "Q5c_left", "Q5d_left", "Q5e_left", "Q5f_left")]
 # worst <- tech.data[, c("Q5a_right", "Q5b_right", "Q5c_right", "Q5d_right", "Q5e_right", "Q5f_right")]
 # names <- c("Apple", "Microsoft", "IBM", "Google", "Intel", "Samsung", "Sony", "Dell", "Yahoo", "Nokia")
-
+#
 # test_that("HB", {
 #     result <- FitMaxDiff(design = tech.design, version = rep(1, nrow(best)), best = best, worst = worst,
 #                          alternative.names = names, algorithm = "HB")
