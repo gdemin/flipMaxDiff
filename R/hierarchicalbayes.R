14f57959584b860e4c012d04efc8c4c18088f1be
JustinCCYap <justinyapcc@gmail.com>
2017-08-08 16:11:49 +1000

    DS-1398: remove stanmodel from result

--- R/hierarchicalbayes.R
+++ R/hierarchicalbayes.R
@@ -47,6 +47,11 @@ hierarchicalBayesMaxDiff <- function(dat, n.iterations = 100, n.chains = 1, is.t
         # where model.code is the stan code as a string.
         # Ideally we would want to recompile when the package is built (similar to Rcpp)
         suppressWarnings(stan.fit <- sampling(mod, data = stan.dat, chains = n.chains, iter = n.iterations))
+        # Replace stanmodel with a dummy as stanmodel makes the output many times larger,
+        # and is not required for diagnostic plots.
+        dummy.stanmodel <- c()
+        class(dummy.stanmodel) <- "stanmodel"
+        stan.fit@stanmodel <- dummy.stanmodel
     }
     else # windows
     {
